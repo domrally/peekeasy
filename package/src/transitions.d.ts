e2dbd68d03cd1b1dc9715c0628ba5d0d27b9dcb8
Dom <dominicemandy@gmail.com>
2021-07-03 01:43:05 -0700

    whoops

--- package/src/transitions.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type Transitions<S> = {
-    [key: number]: [S, S][];
-};
-export declare class TransitionMap<S, T extends number> extends Map<T, Map<S, S>> {
-    constructor(triggers: Transitions<S>);
-}
