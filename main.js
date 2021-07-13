38a2c5609eb54c0af93ef3c6f24a596bbf21d24d
Dom <dominicemandy@gmail.com>
2021-07-13 03:26:51 -0700

    lol

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import { createContext } from './src/context.js';
 import { composeState, State } from './src/state.js';
 import { createTransitions } from './src/transitions.js';
 // 
+if (typeof window !== 'undefined') {
+    (window['mealtimeVersions'] || (window['mealtimeVersions'] = [])).push('0.0.9');
+}
+// 
 const createProxy = (initialState, transitions) => {
     const transitionMap = createTransitions(transitions);
     const handler = createContext(initialState, transitionMap);
--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@ import { composeState, Machineable, State } from './src/state.js'
 import { createTransitions } from './src/transitions.js'
 import { createTriggers } from './src/triggers.js'
 // 
+if (typeof window !== 'undefined') {
+	((window as any)['mealtimeVersions'] || ((window as any)['mealtimeVersions'] = [])).push('0.0.9')
+}
+// 
 const createProxy = <S, T extends symbol>(initialState: S & Machineable & AsyncIterable<T>, transitions: Record<T, [S & Machineable & AsyncIterable<T>, S & Machineable & AsyncIterable<T>][]>) => {
 	const transitionMap = createTransitions(transitions)
 	const handler = createContext(initialState, transitionMap)
