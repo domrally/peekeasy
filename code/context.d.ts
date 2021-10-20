bb83a66b9932b3f69ae9225bd4cbe9f39b6ba364
Dom <dominicemandy@gmail.com>
2021-10-20 14:14:44 -0700

    v0.0.18-alpha

--- code/context.d.ts
+++ code/context.d.ts
@@ -2,7 +2,7 @@ declare type Key = string | number | symbol;
 declare type Property = [key: Key, value: any];
 export declare class Context<T> {
     #private;
-    get proxy(): any;
+    get proxy(): T;
     setTarget(target: T): void;
     observe(): AsyncGenerator<Property, void, unknown>;
     get(_: T, key: Key): any;
