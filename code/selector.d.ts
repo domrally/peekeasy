754647f75112d143e8ced6b183f453af99ac07ae
Dom <dominicemandy@gmail.com>
2021-11-28 23:32:02 -0800

    out with the old

--- code/selector.d.ts
+++
@@ -1,2 +0,0 @@
-export declare type Selector<S extends {}> = ProxyHandler<S> & ((s?: S) => void);
-export declare function selector<S extends {}>(): Selector<S>;
--- code/sender.d.ts
+++
@@ -1,3 +0,0 @@
-import { Action } from "./action.js";
-export declare type Sender = WeakSet<Action> & Action;
-export declare function sender(): Sender;
