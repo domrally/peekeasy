50496390d3367dbe136911965924ef1aa585bcfb
Dom <dominicemandy@gmail.com>
2022-06-11 01:32:53 -0700

    moving

--- src/tests/unit/unit.ts
+++ src/tests/unit/unit.ts
@@ -1,7 +1,7 @@
 import { warn } from 'console'
 import './delegate/call.test'
 warn('✅ call')
-import './delegate/callbacks-delete-async.test'
+import './stream/callbacks-delete-async.test'
 warn('✅ callbacks-delete-async')
 import './delegate/callbacks-delete.test'
 warn('✅ callbacks-delete')
