4501c062ad7f726994e41a8e4a2dfb0eada4073b
Dom <dominicemandy@gmail.com>
2022-06-10 22:36:29 -0700

    remove

--- src/exports/template.ts
+++
@@ -1,9 +0,0 @@
-type Mapped<T> = T
-const Mapped = function () {} as unknown as new <T>() => Mapped<T>
-
-class Example {
-	hello = 'world'
-}
-
-const example = new Mapped<Example>()
-example.hello // the compiler thinks it is an Example
