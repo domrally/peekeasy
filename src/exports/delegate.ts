b6c4c183eba79832febc9bb161c46a6c260784f6
Dom <dominicemandy@gmail.com>
2022-06-11 00:12:44 -0700

    initial

--- src/exports/delegate.ts
+++ src/exports/delegate.ts
@@ -1,11 +1,15 @@
 import { Action } from './exports'
 
 /**
- * function that sends a message to all listeners
+ * A function that sends a message to all set members
  * @param args tuple of data that is passed to the listeners
  */
 export type Delegate<params extends any[]> = Action<params> &
 	Set<Action<params>>
+/**
+ * Delegate constructor
+ * @param initial optional data that can be passed to the listeners immediately
+ */
 export const Delegate = function <params extends any[]>(initial?: params) {
 	const set: any = new Set<Action<params>>(),
 		delegate: any = (...args: params) => {
@@ -16,6 +20,8 @@ export const Delegate = function <params extends any[]>(initial?: params) {
 		const result = set.add(value)
 
 		delegate.size = set.size
+
+		if (initial) value(...initial)
 
 		return result
 	}
