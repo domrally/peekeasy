b2ffe23b171744cd86f7b0f6fedc5cfe488068ec
Dom <dominicemandy@gmail.com>
2022-08-09 15:50:46 -0700

    Update delegate.then.test.ts

--- src/tests/unit/delegate/delegate.then.test.ts
+++ src/tests/unit/delegate/delegate.then.test.ts
@@ -2,10 +2,21 @@ import { Delegate, Forward } from '../../../exports/exports'
 import { test } from '../../test.test'
 
 async function delegateThen() {
-	const forward = new Forward(true),
+	let //
+		is = false,
+		f = (_: boolean) => {},
+		forward = new Forward(f),
 		delegate = new Delegate(forward)
 
-	const [is] = await delegate
+	;(async () => {
+		;[is] = await delegate
+	})()
+
+	await new Promise(resolve => setTimeout(resolve, 0))
+
+	forward(true)
+
+	await new Promise(resolve => setTimeout(resolve, 0))
 
 	return is
 }
