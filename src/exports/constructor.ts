d39a74ca9c92e3c6494c5502c2fea20de03135ac
Dom <dominicemandy@gmail.com>
2022-08-09 15:21:27 -0700

    Delete constructor.ts

--- src/exports/constructor.ts
+++
@@ -1,8 +0,0 @@
-export type Constructor<T, params extends any[] = []> = new (
-	...initial: params
-) => T
-export const Constructor = function (f: unknown) {
-	return f
-} as unknown as new <T, params extends any[] = []>(
-	f: (...initial: params) => T
-) => Constructor<T, params>
